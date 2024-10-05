259d58f5a593b9aa5da1964de68894795cc6759e
MathiasDPX <mathias@dupeux.net>
2024-10-05 14:46:28 +0200

    Add plugin base

---
+++ src/main/java/gg/gyro/example/ExamplePlugin.java
@@ -0,0 +1,17 @@
+package gg.gyro.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExamplePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
